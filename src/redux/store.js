109c16a5c056c09ac71eeb88009b58d2ceb6bb6b
olga-zak <olgaliam@gmail.com>
2023-01-11 01:31:26 +0200

    add ReduxPersist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,29 +1,32 @@
-import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
-//import { devToolsEnhancer } from '@redux-devtools/extension';
-// import { rootReducer } from './reducer';
 import contactsReducer from './contacts/contactsSlice';
 import filterReducer from './filter/filterSlice';
 
-// const rootReducer = combineReducers({
-//   contacts: contactsReducer,
-//   filter: filterReducer,
-// });
+const persistConfig = {
+  key: 'contacts',
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, contactsReducer);
 
-// const persistConfig = {
-//   key: 'contacts',
-//   storage,
-// };
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-// Создаем расширение стора чтобы добавить инструменты разработчика
-//const enhancer = devToolsEnhancer();
-//export const store = createStore(rootReducer, enhancer);
-//export const store = createStore(persistedReducer, enhancer);
 export const store = configureStore({
-  reducer: { contacts: contactsReducer, filter: filterReducer },
+  reducer: { contacts: persistedReducer, filter: filterReducer },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
-//export const persistor = persistStore(store);
+export const persistor = persistStore(store);
